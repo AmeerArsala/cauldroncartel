33ef4a3f7c9465c03fe0a37e831b109025e3cd26
Ameer Arsala <ameer.arsala03@gmail.com>
2024-04-16 00:13:16 -0700

    something 2

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -32,31 +32,38 @@ def purchase_barrels(barrels_delivered: list[Barrel]):
     total_green_ml = 0
     total_red_ml = 0
     total_blue_ml = 0
-    total_price = 0
-    for barrel in barrels_delivered:
-        green_proportion = barrel.potion_type[consts.GREEN] / 100.0
-        green_ml = green_proportion * barrel.ml_per_barrel * barrel.quantity
 
-        total_green_ml += green_ml
+    total_price = 0
+
+    # Calculate totals
+    def calculate_barrel_totals(barrel: Barrel, potion_type: int):
+        proportion = barrel.potion_type[potion_type] / 100.0
+        ml = proportion * barrel.ml_per_barrel * barrel.quantity
+
+        return ml
+
+    for barrel in barrels_delivered:
+        total_green_ml += calculate_barrel_totals(barrel, consts.GREEN)
+        total_red_ml += calculate_barrel_totals(barrel, consts.RED)
+        total_blue_ml += calculate_barrel_totals(barrel, consts.BLUE)
+
         total_price += barrel.price * barrel.quantity
 
-    # Update the DB
+    # Update the DB by diminishing gold and increasing ml (barrels)
     with db.engine.begin() as conn:
         select_result = conn.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         row = select_result[0]
 
-        # Postprocess the barrels into potions
         current_green_ml = row["num_green_ml"] + total_green_ml
-        num_added_potions = int(current_green_ml / consts.ML_PER_BOTTLE)
+        current_red_ml = row["num_red_ml"] + total_red_ml
+        current_blue_ml = row["num_blue_ml"] + total_blue_ml
 
-        remaining_green_ml = current_green_ml % consts.ML_PER_BOTTLE
         current_gold = row["gold"] - total_price
-        current_green_potions = row["num_green_potions"] + num_added_potions
 
         # Update statement
         conn.execute(
             sqlalchemy.text(
-                f"UPDATE global_inventory SET gold = {current_gold}, num_green_ml = {remaining_green_ml},"
+                f"UPDATE global_inventory SET gold = {current_gold}, num_green_ml = {current_green_ml}, num_red_ml = {current_red_ml}, num_blue_ml = {current_blue_ml}"
             )
         )
 
